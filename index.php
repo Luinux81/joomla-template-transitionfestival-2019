1b836790162871d9a9001eb75fc322043f999587
Luinux81 <luinux81@gmail.com>
2018-12-16 23:47:03 +0100

    Add files via upload
    Version 1.0

---
+++ index.php
@@ -0,0 +1,172 @@
+<?php
+
+defined('_JEXEC') or die;
+
+$doc = JFactory::getDocument();
+$doc->addStyleSheet($this->baseurl . '/media/jui/css/bootstrap.min.css');
+$doc->addStyleSheet($this->baseurl . '/media/jui/css/bootstrap-responsive.css');
+$doc->addStyleSheet('templates/' . $this->template . '/css/style.css');
+$doc->addScript('templates/' . $this->template . '/js/main.js', 'text/javascript');
+
+$app = JFactory::getApplication();
+$menu = $app->getMenu();
+$lang = JFactory::getLanguage();
+if ($menu->getActive() == $menu->getDefault($lang->getTag())) {
+	$frontpage=true;
+}
+else {
+	$frontpage=false;
+}
+
+$path_template   = JURI::base(true).'/templates/'.$app->getTemplate().'/';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>
+    <jdoc:include type="head" />
+    <?php //<meta name="viewport" content="width=device-width, initial-scale=1.0"> ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+  	
+    <meta property="og:image" content="http://www.transitionfestival.org/images/2019/transition_logo_multi.jpg"/>
+    <meta property="og:title" content="Transition Festival"/>
+
+  	
+	<link href="https://fonts.googleapis.com/css?family=Mystery+Quest" rel="stylesheet">
+
+	
+</head>
+<body>  	
+
+<?php if(true){ ?>
+
+<div id="div-imagen-rotatoria">
+	<img src="<?php echo $path_template . "images/fondo_rot.png" ?>" id="imagen-rotatoria">
+</div>
+
+<div id="div-menu-mobile" class="sidenav ">
+	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+	<a href="./index.php"><img id="logo-central-mobile" src="<?php echo $path_template . "images/logo_central.png" ?>"/></a>
+	<a class="fuenteMenu" href="./index.php?video=0">HOME</a>
+	<a id="menuitem-mob-festival" class="fuenteMenu" href="./index.php?option=com_content&view=category&id=8">FESTIVAL</a>
+	<a id="menuitem-mob-guia" class="fuenteMenu" href="./index.php?option=com_content&view=category&id=9">GUIA</a>
+	<a id="menuitem-mob-lugar" class="fuenteMenu" href="./index.php?option=com_content&view=category&id=10">LUGAR</a>
+	<a id="menuitem-mob-entradas" class="fuenteMenu menuitem-destacado" href="./index.php?option=com_content&view=article&id=5">ENTRADAS</a>
+	<a id="menuitem-mob-galeria" class="fuenteMenu" href="./index.php?option=com_content&view=article&id=107" >GALERIA</a>
+	<a id="menuitem-mob-contacto" class="fuenteMenu" href="./index.php?option=com_content&view=category&id=11">CONTACTO</a>
+</div>
+
+<div id="contenedorTop" class="">
+	<div id="div-menu-mobile-icon">
+		<img id="menu-mobile-icon" src="<?php echo $path_template . "images/menu_icon.png" ?>" onclick="openNav()"/>
+	</div>
+	
+	<div id="div-top-news">
+		<a href="./index.php?video=0" >	<img src="<?php echo $path_template . "images/news.png" ?>" /> </a>
+	</div>
+	<div id="div-top-menu">
+		<div id="div-top-menu-izq">
+			<a id="menuitem-festival" class="menuitem-nav-link fuenteMenu" href="./index.php?option=com_content&view=category&id=8">FESTIVAL</a>
+			<a id="menuitem-guia" class="menuitem-nav-link fuenteMenu" href="./index.php?option=com_content&view=category&id=9">GUIA</a>
+			<a id="menuitem-lugar" class="menuitem-nav-link fuenteMenu" href="./index.php?option=com_content&view=category&id=10">LUGAR</a>
+		</div>
+		<div id="div-top-menu-centro">
+		<a href="./index.php">	<img id="logo-central" src="<?php echo $path_template . "images/logo_central.png" ?>"/>	</a>
+		</div>
+		<div id="div-top-menu-der">
+			<a id="menuitem-entradas" class="menuitem-nav-link menuitem-destacado fuenteMenu" href="./index.php?option=com_content&view=article&id=5">ENTRADAS</a>
+			<a id="menuitem-galeria" class="menuitem-nav-link fuenteMenu" href="./index.php?option=com_content&view=article&id=107" >GALERIA</a>
+			<a id="menuitem-contacto" class="menuitem-nav-link fuenteMenu" href="./index.php?option=com_content&view=category&id=11">CONTACTO</a>
+		</div>
+	</div>
+	<div id="div-top-idioma">
+		<jdoc:include type="modules" name="position-0" style="html5" />
+	</div>
+	<div id="div-back">			
+		<img id="img-back" src="<?php echo $path_template . "images/back.png" ?>" onclick="history.back();" />			
+	</div>
+
+</div>
+	
+	
+<div id="contenedorMid" class="">
+
+	<div id="div-cabecera-home" class="div-cabecera">
+		<div id="div-cabecera-slider" class="div-cabecera">
+			<jdoc:include type="modules" name="position-1" style="html5" />	
+		</div>
+
+		<div id="div-cabecera-info" class="div-cabecera">
+			<div id="div-cabecera-info-izq" class="div-cabecera div-cabecera-info">
+				<span class="info-cabecera-lateral-izq">14-21 MAY 2019</span>
+			</div>
+			<div id="div-cabecera-info-centro" class="div-cabecera div-cabecera-info">
+				<img class="imagen-cabecera-info-centro spin" src="<?php echo $path_template . "images/marco1.png" ?>"></img>	
+				<img class="imagen-cabecera-info-centro <?php //anti-spin ?> imagen-cabecera-transparente transparencia-animada"  src="<?php echo $path_template . "images/marco2.png" ?>"></img>	
+				<div class="texto-cabecera-info-centro">
+					<p class="info-cabecera-central">Quedan <br><span class="dias-restantes">0</span><br>dias</p>
+				</div>
+			</div>
+			<div id="div-cabecera-info-der" class="div-cabecera div-cabecera-info">
+				<span class="info-cabecera-lateral-der">Doñana, Spain</span>
+			</div>
+		</div>
+	</div>
+	
+	<div id="div-contenido">
+		<jdoc:include type="component" />
+	</div>
+	
+</div>
+
+<div id="div-redes-sociales" style="">
+	<a href="https://www.youtube.com/results?search_query=transition+festival" target="_blank" rel="noopener">
+		<img class="img-red-social" src="<?php echo $path_template . "images/redes_sociales/youtube.png" ?>" />
+	</a>
+	<a href="https://www.facebook.com/TransitionFestival/" target="_blank" rel="noopener">
+		<img class="img-red-social" src="<?php echo $path_template . "images/redes_sociales/facebook.png" ?>" />
+	</a>
+	<a href="https://www.instagram.com/transition.festival/" target="_blank" rel="noopener">
+		<img class="img-red-social" src="<?php echo $path_template . "images/redes_sociales/instagram.png" ?>" />
+	</a>
+	<a href="https://twitter.com/transition_crew" target="_blank" rel="noopener">
+		<img class="img-red-social" src="<?php echo $path_template . "images/redes_sociales/twitter.png" ?>" />
+	</a>
+	<a href="<?php //./index.php?option=com_content&view=article&id=119 ?>#" target="_parent">
+		<img class="img-red-social" src="<?php echo $path_template . "images/redes_sociales/email.png" ?>" />
+	</a>
+</div>
+
+<div id="div-overlay" onclick="closeVideo()">
+	<div id="video-contenedor">
+		<a href="javascript:void(0)" id="btnCerrarVideo" onclick="closeVideo()">x</a>
+		<iframe width="100%" height="100%" src="https://www.youtube.com/embed/-2y2MPoDFvI?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+</div>
+<?php 
+	}
+	else{
+?>		
+
+	<div id="div_imagen_previa">
+		<img src="./images/2018/FONDO2019-compressor.jpg" id="imagen_previa_web" style="width:100%;display:none;">
+	</div>
+  
+  	<div id="posicionadorShop" style="position:absolute; top:20%; width:100%;">
+  
+  		<div style="width:75%; text-align:left; margin:0 auto;">
+  			<iframe src="https://eventbrite.com/tickets-external?eid=46356135449&ref=etckt" frameborder="0" height="955" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+  			<div style="font-family:Helvetica, Arial; font-size:12px; padding:10px 0 5px; margin:2px; width:100%; text-align:left;" ><a class="powered-by-eb" style="color: #ADB0B6; text-decoration: none;" target="_blank" href="https://www.eventbrite.com/">Powered by Eventbrite</a></div></div>
+  
+  
+  		<div style="display:none;width:50%;margin:0 auto;">
+  			
+  		</div>  		
+  	</div>
+	
+<?php	} ?>
+
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,209 @@
+jQuery(document).ready(function(){
+	//you can now use $ as your jQuery object.
+	//jQuery(".menuitem-nav-link").addClass("fuenteMenu");  
+	
+	
+	//jQuery("#contenedorTop").hide().fadeIn(1000);
+	
+	if(!mostarVideo()){
+		$("#div-overlay").hide();
+		$("body").css("overflow-y","scroll");
+		$("#video-contenedor > iframe").remove();
+	}
+	
+	jQuery("#contenedorTop a, .item a").click(function(){
+		jQuery("#contenedorTop").fadeOut(750);	
+		jQuery("#div-contenido").fadeOut(500);
+		jQuery("#div-cabecera-home").fadeOut(500);
+		jQuery("#div-redes-sociales").fadeOut(500);
+	});
+	
+	jQuery(".item").hide();
+	
+	jQuery("#div-contenido").hide().fadeIn(1000);
+	
+	var dias=diasRestantes();
+
+	jQuery("#dias-numero").html(dias);
+	
+	$("#contenedorTop").hide().delay(500).fadeIn(500);
+	
+	$("#div-redes-sociales").hide().delay(1000).fadeIn(500);
+	
+	if(mostrarCabecera()){		
+		$("#div-cabecera-home").hide().delay(1000).fadeIn(500);
+		jQuery("#div-back").css("display","none");
+	}
+	else{
+		jQuery("#div-cabecera-home").hide();
+	}
+	
+	jQuery(".item").each(function(index){
+		jQuery(this).delay(1000+(index*500)).fadeIn(1000);
+	});
+	
+	
+	resaltaItemMenuActual();
+	
+	if(!esGaleria()){
+		jQuery(".item-page").parent().addClass("anchoArticulo");
+	}
+	else{
+		jQuery(".item-page").parent().css("width","85% !important");
+		jQuery(".imagegalleryplg img").addClass("imagegallery-sin-marco");
+	}
+ 
+/*	
+	jQuery(".imagegalleryplg img").each(function(){
+		jQuery(this).on('load', function(){
+			jQuery(this).removeClass("imagegallery-sin-marco");
+			jQuery(this).addClass("imagegallery-con-marco");
+		});
+	});
+*/
+	
+	var lang=jQuery(".lang-active a").attr("href");
+	if (lang.search("/es")>-1){
+		jQuery(".menuitem-destacado").attr("href","./index.php?option=com_content&view=article&id=5");		
+		cambiaIdiomaMenu("es");
+		
+	}
+	else if(lang.search("/en")>-1){
+		jQuery(".menuitem-destacado").attr("href","./index.php?option=com_content&view=article&id=108");		
+		cambiaIdiomaMenu("en");
+	}
+	
+});
+
+
+function esHomePage(){
+	var url=window.location.href;
+  
+ 
+  	if( (url.search("fbclid=")>-1) || (url.search("video=0")>-1)  ){      
+      return true;
+    }
+    else{
+    	url=url.slice(-13);
+		return ((url.search("index.php/es")>-1) || (url.search("index.php/en")>-1));      
+    }    
+	
+}
+
+function mostrarCabecera(){
+	var url=window.location.href;
+	
+	if(esHomePage()){
+		return true;
+	}
+	else{
+		if(url.search("option=com_content&view=category")>-1){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
+
+function esGaleria(){
+	var url=window.location.href;
+	
+	return (url.search("option=com_content&view=article&id=107")>-1);
+}
+
+function resaltaItemMenuActual(){
+	var url1 = window.location.href; 
+	var url2 = "";
+
+	url1=url1.substring(url1.lastIndexOf("?"));
+
+	jQuery(".menuitem-nav-link").each(function() {
+		url2=this.href;
+		url2=url2.substring(url2.lastIndexOf("?"));
+
+		//console.log("URL1: "+url1+"\n"+"URL2: "+url2);
+
+		if(url1 == url2) { 
+			jQuery(this).addClass("menuitem-actual");
+			jQuery(this).removeClass("menuitem-destacado");
+		}
+		else{
+			jQuery(this).removeClass("menuitem-actual");
+		}
+	});	
+}
+
+function diasRestantes(){
+	var festival=new Date('2018-04-25').getTime();
+	var ahora=Date.now();
+	var aux=0;
+    
+	var diff = festival - ahora;
+    
+    if(diff<0){
+    	diff=0;        
+    }
+    else{
+    	aux=2;
+    }
+	
+	return(Math.floor(diff/(1000*60*60*24))+aux);	
+}
+
+
+function openNav() {
+    document.getElementById("div-menu-mobile").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("div-menu-mobile").style.width = "0";
+}
+
+function mostarVideo(){
+	var url=window.location.href;
+
+	if(url.search("video=0")>-1){
+		return false;
+	}
+	else{
+		return esHomePage();
+	}
+}
+
+function closeVideo(){
+	var url=window.location.href;
+	window.location.href=url+"?video=0";	
+}
+
+function cambiaIdiomaMenu(lang){
+	if(lang=="es"){
+		jQuery("#menuitem-festival").html("FESTIVAL");
+		jQuery("#menuitem-mob-festival").html("FESTIVAL");
+		jQuery("#menuitem-guia").html("GUIA");
+		jQuery("#menuitem-mob-guia").html("GUIA");
+		jQuery("#menuitem-lugar").html("LUGAR");
+		jQuery("#menuitem-mob-lugar").html("LUGAR");
+		jQuery("#menuitem-entradas").html("ENTRADAS");
+		jQuery("#menuitem-mob-entradas").html("ENTRADAS");
+		jQuery("#menuitem-galeria").html("GALERIA");
+		jQuery("#menuitem-mob-galeria").html("GALERIA");
+		jQuery("#menuitem-contacto").html("CONTACTO");
+		jQuery("#menuitem-mob-contacto").html("CONTACTO");
+	}
+	else{
+		jQuery("#menuitem-festival").html("FESTIVAL");
+		jQuery("#menuitem-mob-festival").html("FESTIVAL");
+		jQuery("#menuitem-guia").html("GUIDE");
+		jQuery("#menuitem-mob-guia").html("GUIDE");
+		jQuery("#menuitem-lugar").html("LOCATION");
+		jQuery("#menuitem-mob-lugar").html("LOCATION");
+		jQuery("#menuitem-entradas").html("TICKETS");
+		jQuery("#menuitem-mob-entradas").html("TICKETS");
+		jQuery("#menuitem-galeria").html("GALLERY");
+		jQuery("#menuitem-mob-galeria").html("GALLERY");
+		jQuery("#menuitem-contacto").html("CONTACT");
+		jQuery("#menuitem-mob-contacto").html("CONTACT");
+	}
+}
+
